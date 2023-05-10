cc43562062241767852eb1b7df2be9bb49ff7519
Spencer Hahmo <schahmo@gmail.com>
2023-05-10 13:01:43 -0600

    Refactored code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,16 +10,16 @@ RSpec.describe Product, type: :model do
     end
 
     it "validates that there must be a name for the product" do
-      @categories = Category.new(:name => "test")
-      @product = Product.new(:name => nil, :price => 249.99, :quantity => 5, :category => @categories)
+      @categories = Category.new(name: "test")
+      @product = Product.new(name: nil, price: 249.99, quantity: 5, category: @categories)
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to_not be_empty
       expect(@product.errors.full_messages[0]).to eq("Name can't be blank")
     end
 
     it "validates that there must be a price for the product" do
-      @categories = Category.new(:name => "test")
-      @product = Product.new(:name => "Evergreen", :price => nil, :price_cents => nil, :quantity => 5, :category => @categories)
+      @categories = Category.new(name: "test")
+      @product = Product.new(name: "Evergreen", price: nil, price_cents: nil, quantity: 5, category: @categories)
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to_not be_empty
       expect(@product.errors.full_messages[0]).to eq("Price cents is not a number")
@@ -28,15 +28,15 @@ RSpec.describe Product, type: :model do
     end
 
     it "validates that there must be a quantity for the product" do
-      @categories = Category.new(:name => "test")
-      @product = Product.new(:name => "Evergreen", :price => 249.99, :quantity => nil, :category => @categories)
+      @categories = Category.new(name: "test")
+      @product = Product.new(name: "Evergreen", price: 249.99, quantity: nil, category: @categories)
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to_not be_empty
       expect(@product.errors.full_messages[0]).to eq("Quantity can't be blank")
     end
 
     it "validates that there must be a category for the product" do
-      @product = Product.new(:name => "Evergreen", :price => 249.99, :quantity => 5, :category => nil)
+      @product = Product.new(name: "Evergreen", price: 249.99, quantity: 5, category: nil)
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to_not be_empty
       expect(@product.errors.full_messages[0]).to eq("Category must exist")
