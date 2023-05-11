dbad47186659429ef02df6da513579e0d365576a
Spencer Hahmo <schahmo@gmail.com>
2023-05-11 14:15:02 -0600

    Feature Spec: Product Details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,8 +1,8 @@
-describe('home page', () => {
+describe('product details page', () => {
 
   it('can visit the homepage', () => {
     cy.visit('http://localhost:3000/')
-  })
+  });
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
@@ -11,5 +11,12 @@ describe('home page', () => {
   it("There is 12 products on the page", () => {
     cy.get(".products article").should("have.length", 12);
   });
+
+  it('lets you visit the selected product detail page', () => {
+    cy.visit('/')
+    cy.get(".products article").first().click()
+    cy.url().should("include", "/products/2")
+  
+  })
 
 });
